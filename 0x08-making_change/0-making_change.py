f386119dd7d9d0819168e15c9be0549dbd39f1ad
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-08-22 11:31:27 +0000

    new commit

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,13 +4,11 @@
 
 def makeChange(coins, total):
     """Return: fewest number of coins needed to meet total"""
-    if total <= 0:
-        return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    coins.sort(reverse=True)
+    count = 0
     for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+        if total == 0:
+            break
+        count += total // coin
+        total %= coin
+    return count if total == 0 else -1
