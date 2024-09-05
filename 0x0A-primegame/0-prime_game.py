75b8b0b62b1bf2286c931f94d19791a1ae650b6f
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-09-05 11:25:25 +0000

    new commit

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""Prime Game"""
+
 
 def sieve_of_eratosthenes(max_n):
+    """
+    Generate a list of prime numbers up to max_n.
+
+    Args:
+    max_n (int): The upper limit for generating prime numbers.
+
+    Returns:
+    List[int]: A list of prime numbers up to max_n.
+    """
     is_prime = [True] * (max_n + 1)
     p = 2
     while (p * p <= max_n):
-        if (is_prime[p]):
+        if (is_prime[p] is True):
             for i in range(p * p, max_n + 1, p):
                 is_prime[i] = False
         p += 1
@@ -13,6 +24,16 @@ def sieve_of_eratosthenes(max_n):
 
 
 def count_primes_upto_n(n, primes):
+    """
+    Count the number of prime numbers less than or equal to n.
+
+    Args:
+    n (int): The upper limit to count primes.
+    primes (List[int]): The list of precomputed prime numbers.
+
+    Returns:
+    int: The count of prime numbers up to n.
+    """
     count = 0
     for prime in primes:
         if prime <= n:
@@ -23,6 +44,17 @@ def count_primes_upto_n(n, primes):
 
 
 def isWinner(x, nums):
+    """
+    Determine the winner of the prime number game after x rounds.
+
+    Args:
+    x (int): The number of rounds.
+    nums: A list of integers representing the upper limit for each round.
+
+    Returns:
+    str or None: The name of the player with the most wins ('Maria' or 'Ben').
+                 If the game is a tie, return None.
+    """
     max_n = max(nums)
     primes = sieve_of_eratosthenes(max_n)
 
